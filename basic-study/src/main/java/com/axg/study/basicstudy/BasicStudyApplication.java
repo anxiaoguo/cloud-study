25b060a19b4a617efbf015d74706f28cc0b20c03
anxiaoguo <3063026004@qq.com>
2022-02-24 16:09:25 +0800

    第一次提交

---
+++ basic-study/src/main/java/com/axg/study/basicstudy/BasicStudyApplication.java
@@ -0,0 +1,13 @@
+package com.axg.study.basicstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicStudyApplication.class, args);
+    }
+
+}
---
+++ basic-study/src/test/java/com/axg/study/basicstudy/BasicStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axg.study.basicstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
