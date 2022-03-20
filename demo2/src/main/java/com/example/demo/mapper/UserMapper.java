919182c3686e1bc9f965b063e4161680c2c35d27
anxiaoguo <3063026004@qq.com>
2022-03-20 19:28:57 +0800

    66666666

--- demo2/src/main/java/com/example/demo/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.axg.springboot.demo.mapper;
-
-import com.axg.springboot.demo.model.User;
-
-public interface UserMapper {
-
-    User selectUserById(Integer id);
-}
--- demo2/src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.axg.springboot.demo.service;
-
-import com.axg.springboot.demo.model.User;
-
-public interface UserService {
-
-    User selectUserById(Integer id);
-}
